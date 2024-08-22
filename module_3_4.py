539a8155f63a2566152b3c9aa099f6594cdfdca8
zoleg-oi <zoleg@mail.ru>
2024-08-22 11:14:24 +0300

    Add files via upload

--- module_3_4.py
+++ module_3_4.py
@@ -8,6 +8,7 @@ def single_root_words_ower(metod, root_word, *other_words):
     # метод решения зависит от переданного параметра
     # 0 - используем оператор in
     # 1 - используем метод find()
+    # 2 - используем метод index()
     same_words = []
     root_word = root_word.lower()
 
@@ -29,14 +30,33 @@ def single_root_words_ower(metod, root_word, *other_words):
             if root_word.find(ow) != -1:  same_words.append(other_words[i])
             # а теперь наоборот не входит ли элемент в root
             if ow.find(root_word) != -1:  same_words.append(other_words[i])
+    if metod == 2:
+        print()
+        print('Используем метод - index()')
+        for i in range(len(other_words)):
+            ow = other_words[i].lower()
+            try:
+                root_word.index(ow)
+            except:
+                continue #ничего не делаем
+            else:
+                same_words.append(other_words[i])
+            try:
+                ow.index(root_word)
+            except:
+                continue #ничего не делаем
+            else:
+                same_words.append(other_words[i])
 
     return same_words
 
 
 result1 = single_root_words_ower(1, 'rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 print(result1)
-result2 = single_root_words_ower(0, 'Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+result2 = single_root_words_ower(2, 'Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 print(result2)
+result3 = single_root_words_ower(0, 'Ablemable', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result3)
 print()
 
 print('Решение, указанное в задании')
